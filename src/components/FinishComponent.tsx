db89246316bf6e4a36c80c265245a47b76e8edfa
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-09-23 18:18:15 +0200

    Add background color in finish screen

--- src/components/FinishComponent.tsx
+++ src/components/FinishComponent.tsx
@@ -1,9 +1,13 @@
 import { Winner } from "../types";
 
 export const FinishComponent = ({ winner }: Winner) => {
+  const background = winner === "Player1" ? "yellow" : "red";
+
   return (
     <div className="startWrapper">
-      <h1>{winner} won!</h1>
+      <h1>
+        <span style={{ background }}>{winner}</span> won!
+      </h1>
     </div>
   );
 };
